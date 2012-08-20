090b458e4842973434d9bdee1ba1f4870813b2a7
Vincent Stehlé <v-stehle@ti.com>
2012-08-20 10:59:27 +0200

    Integrate PSI KPI profiler
    This is PSI KPI profiler integration into the DCE image, take 3.
    
    We track create/delete to support more than one DCE stream running with correct
    profiling.
    
    We report KPI IVA & cores loads in the load_task. We hack PSI KPI profiler a
    bit, to be able to read the IVA & cores total times "live" and do our
    computations. We protect the cores statistical data collection with a GateAll
    for consistency. Note that each core has his timer now.
    
    Adapt some functions for latest bios:
    	s/Core_getCoreId/Core_getId/
    	s/Hwi_disableCoreInts/Hwi_disable/
    	s/Hwi_restoreCoreInts/Hwi_restore/
    
    We also pass chipset_id to the KPI profiler for runtime OMAP selection. This is
    used only for 32k timer address determination right now, but the mechanism is
    in place, at least.
    
    Remainings things:
    
    - Do not hardcode KPI_PROFILER but let the build "recipe" decide.
    - Make the create/delete refcnt work with other codec "clients" than DCE, too.
    - Fix profile.h include path; today this breaks DCE "modularity".
    - Dynamic enable/disable (print phy addr of kpi_control and let the user poke?)
      At least do not touch from dce.c
    - Do not hardcode BUILD_FOR_SMP?
    - Come back to per-core interrupts disable/enable when bios allows it again.
    
    Signed-off-by: Vincent Stehlé <v-stehle@ti.com>

--- ducati/ti/dce/dce.c
+++ ducati/ti/dce/dce.c
@@ -65,8 +65,14 @@
 #define DCE_PORT 42     /* so long, and thanks for all the fish */
 
 uint32_t dce_debug = 1;
+uint32_t dce_chipset_id;
 
 #define MEMORYSTATS_DEBUG
+#define KPI_PROFILER
+
+#ifdef KPI_PROFILER
+# include "baseimage/profile.h"
+#endif
 
 /* AFAIK both TILER and heap are cached on ducati side.. so from wherever a9
  * allocates, we need to deal with cache to avoid coherency issues..
@@ -96,6 +102,11 @@ static XDAS_Int32 videnc2_control(VIDENC2_Handle codec, VIDENC2_Cmd id, VIDENC2_
 static VIDDEC3_Handle viddec3_create(Engine_Handle engine, String name, VIDDEC3_Params *params);
 static int videnc2_reloc(VIDDEC3_Handle handle, uint8_t *ptr, uint32_t len);
 static int viddec3_reloc(VIDDEC3_Handle handle, uint8_t *ptr, uint32_t len);
+
+#ifdef KPI_PROFILER
+/* track codec create/delete usage */
+static int create_count = 0;
+#endif
 
 static struct {
     CreateFxn  create;
@@ -240,6 +251,7 @@ static int connect(void *msg)
     struct dce_rpc_connect_req *req = msg;
 
     DEBUG(">> chipset_id=0x%x, debug=%d", req->chipset_id, req->debug);
+    dce_chipset_id = req->chipset_id;
     dce_debug = req->debug;
 
     if (dce_debug <= 1) {
@@ -290,6 +302,10 @@ static int engine_close(void *msg)
 
 static int codec_create(void *msg)
 {
+#ifdef KPI_PROFILER
+    extern unsigned long kpi_control;
+#endif
+
 #ifdef MEMORYSTATS_DEBUG
     Memory_Stats stats;
 #endif
@@ -305,6 +321,14 @@ static int codec_create(void *msg)
     dce_clean(sparams);
     DEBUG("<< codec=%08x", rsp->codec);
 
+#ifdef KPI_PROFILER
+    /* We initialize only for the first create. TODO: make it work when other
+     * task can create codecs, too. */
+    if (create_count++ == 0) {
+        kpi_control = KPI_END_SUMMARY /*| KPI_IVA_DETAILS | KPI_CPU_DETAILS*/;
+        kpi_instInit(dce_chipset_id);
+    }
+#endif
 #ifdef MEMORYSTATS_DEBUG
     Memory_getStats(NULL, &stats);
     INFO("Total: %d\tFree: %d\tLargest: %d", stats.totalSize,
@@ -468,8 +492,18 @@ static int codec_process(void *msg)
 
     if (rsp->result == IALG_EOK) {
         ivahd_acquire();
+
+#ifdef KPI_PROFILER
+        kpi_before_codec(dce_chipset_id);
+#endif
+
         rsp->result = codec_fxns[codec_id].process(
                 (void *)req->codec, in_bufs, out_bufs, in_args, out_args);
+
+#ifdef KPI_PROFILER
+        kpi_after_codec();
+#endif
+
         ivahd_release();
     } else {
         DEBUG("reloc failed");
@@ -517,6 +551,13 @@ static int codec_delete(void *msg)
     codec_fxns[req->codec_id].delete((void *)req->codec);
     DEBUG("<<");
 
+#ifdef KPI_PROFILER
+    /* We de-initialize only for the last delete. TODO: make it work when other
+     * task can delete codecs, too. */
+    if (--create_count == 0) {
+        kpi_instDeinit();
+    }
+#endif
 #ifdef MEMORYSTATS_DEBUG
     Memory_getStats(NULL, &stats);
     INFO("Total: %d\tFree: %d\tLargest: %d", stats.totalSize,
